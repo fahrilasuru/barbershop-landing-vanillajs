234b000cff3bcc7c4e09d1ec4de072f85e77cb1c
fahrilasuru <fahriz.lasuru@gmail.com>
2026-08-05 00:28:31 +0700

    feat: add styles section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 import { renderTemplate } from "./utils.js";
 
 renderTemplate({
-    containerSelector: ".barber-grid",
+    containerSelector: ".barbers-grid",
     templateId: "barber-card-template",
     items: [
-        { avatar: ["src", "images/barber_1.webp"], name: "Andre", role: "Head Barber", rating: "★★★★★" },
-        { avatar: ["src", "images/barber_2.webp"], name: "Bima", role: "Spesialis Beard", rating: "★★★★★" },
-        { avatar: ["src", "images/barber_3.webp"], name: "Candra", role: "Hair Care", rating: "★★★★☆" },
-        { avatar: ["src", "images/barber_4.webp"], name: "Dimas", role: "Hair Coloring", rating: "★★★★★" },
+        { avatar: ["src", "images/barber_1.webp"], name: "Andre", role: "Head Barber", rating: "★★★★★", },
+        { avatar: ["src", "images/barber_2.webp"], name: "Bima", role: "Spesialis Beard", rating: "★★★★★", },
+        { avatar: ["src", "images/barber_3.webp"], name: "Candra", role: "Hair Care", rating: "★★★★☆", },
+        { avatar: ["src", "images/barber_4.webp"], name: "Dimas", role: "Hair Coloring", rating: "★★★★★", },
+    ],
+});
+
+renderTemplate({
+    containerSelector: ".styles-grid",
+    templateId: "style-card-template",
+    items: [
+        { photo: ["src", "images/two_block_cut.webp"], name: "Two Block Cut", },
+        { photo: ["src", "images/buzz_cut.webp"], name: "Buzz Cut", },
+        { photo: ["src", "images/undercut.webp"], name: "Undercut", },
+        { photo: ["src", "images/textured_crop.webp"], name: "Textured Crop", },
+        { photo: ["src", "images/french_crop.webp"], name: "French Crop", },
+        { photo: ["src", "images/comma_hair.webp"], name: "Comma Hair", },
+        { photo: ["src", "images/slick_back.webp"], name: "Slick Back", },
+        { photo: ["src", "images/mullet.webp"], name: "Mullet", },
     ],
 });
